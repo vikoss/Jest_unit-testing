62dc365a3a252b6b78230de355de947ac2c080af
vikoss <vikoss.yiss@gmail.com>
2021-08-06 11:33:45 -0500

    add more test with jest 🤩️

--- __test__/maths.test.js
+++ __test__/maths.test.js
@@ -9,4 +9,13 @@ describe('Basic operations', () => {
   test('two plus two is four', () => {
     expect(add(2, 2)).toBe(4);
   });
+  test('two subtract two is minus two', () => {
+    expect(subtract(5, 7)).toBe(-2);
+  });
+  test('five multiply two is ten', () => {
+    expect(multiply(5, 2)).toBe(10);
+  });
+  test('ten divide five is two', () => {
+    expect(divide(10, 5)).toBe(2);
+  });
 });
--- maths.js
+++ maths.js
@@ -1,22 +1,28 @@
-const add = (a, b) => {
-  return a + b;
-};
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
-const subtract = (a, b) => {
-  return a - b;
-};
+const isNull = () => null;
+const isTrue = () => true;
+const isFalse = () => false;
+const isUndefined = () => undefined;
 
-const multiply = (a, b) => {
-  return a * b;
-};
+const colors = () => ['red', 'blue', 'green'];
+const colors1 = () => ['red', 'blue', 'green'];
 
-const divide = (a, b) => {
-  return a / b;
-};
+const callbackMain = (callback) => callback('Hello');
 
 export {
   add,
   subtract,
   multiply,
   divide,
+  isNull,
+  isTrue,
+  isFalse,
+  isUndefined,
+  colors,
+  colors1,
+  callbackMain,
 };
